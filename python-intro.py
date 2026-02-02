4c045ec80b8a5ad2a841691b0f9d49723b5a5491
Petra Elrod <petra_elrod@mines.edu>
2026-02-02 15:20:26 -0700

    Add my first python script

---
+++ python-intro.py
@@ -0,0 +1,10 @@
+# Learning how to write a Python script - GPGN268
+# Author: Petra Elrod
+# Date: Feb 02 2026
+
+# Temperature in degrees Fahrenheit
+temperature = 55
+# Location of the measurement
+city = "Golden"
+print("It is", temperature, "F", "in", city)
+
